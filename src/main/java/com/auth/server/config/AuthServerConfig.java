1d6e4d6505451aa641a450726f8cfbcfc131c421
Zakariae El Hichem <zakariae.el.hichem@desjardins.com>
2023-03-21 11:21:26 -0400

    Issue JWT with symetric key

--- src/main/java/com/auth/server/config/AuthServerConfig.java
+++ src/main/java/com/auth/server/config/AuthServerConfig.java
@@ -1,6 +1,8 @@
 package com.auth.server.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -8,18 +10,18 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Value("${jwt.key}")
+    private String jwtKey;
     @Autowired
     private AuthenticationManager authenticationManager;
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager);
-    }
 
     // password grant type
     @Override
@@ -36,7 +38,6 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .and()
                 .withClient("resourceserver")
                 .secret("resourceserversecret");
-        ;
     }
 
     // authorization code grant type
@@ -61,8 +62,28 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .authorizedGrantTypes("client_credentials")
                 .scopes("info");
     }*/
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore())
+                .accessTokenConverter(
+                        jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        var converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(jwtKey);
+        return converter;
+    }
+
     public void configure(AuthorizationServerSecurityConfigurer security) {
-        security.checkTokenAccess
-                ("isAuthenticated()");
+        security.checkTokenAccess("isAuthenticated()");
     }
 }
